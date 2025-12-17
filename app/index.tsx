73ec863854a0c50efb37b3e78e7ff612fcb57241
dilini <dilinichamikasilva@gmail.com>
2025-12-17 09:10:27 +0530

    feat:init the project

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import "../global.css"
+
+
+const index = () => {
+  return (
+    <View>
+      <Text>Hello there👋</Text>
+    </View>
+  )
+}
+
+export default index
