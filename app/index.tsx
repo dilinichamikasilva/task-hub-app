224332b984563f2a1e9e8f720e294d2cb6876f24
dilini <dilinichamikasilva@gmail.com>
2026-01-07 14:45:23 +0530

    feat: implement loading state and user redirection in index component

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,25 @@
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, ActivityIndicator } from 'react-native'
 import React from 'react'
 import "../global.css"
 import { Redirect } from 'expo-router'
+import { useAuth } from '@/hooks/useAuth'
 
 const index = () => {
-    return (
-        <Redirect href="/login" />
-    )
+    const {user , loading} = useAuth()
+
+    if(loading){
+        return <View className="flex-1 justify-center items-center">
+            <ActivityIndicator size="large" color="#1e40af" />
+        </View>
+    }
+
+    if(user){
+        return (<Redirect href="/home" />)
+    }else{
+        return (<Redirect href="/login" />)
+    }
+
+    
 }
 
 export default index
