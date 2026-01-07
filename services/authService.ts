3baf0a39cf3e40487f2004de95787ddc3215473b
dilini <dilinichamikasilva@gmail.com>
2026-01-07 14:51:46 +0530

    feat: add logout functionality with AsyncStorage clearance

--- services/authService.ts
+++ services/authService.ts
@@ -1,6 +1,8 @@
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile , } from "firebase/auth";
 import { auth, db } from "./fireBaseConfig";
 import { doc , setDoc } from "firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { signOut } from "firebase/auth";
 
 export const registerUser = async (
     fullname : string , 
@@ -27,4 +29,10 @@ export const registerUser = async (
 
 export const loginUser = async(email : string , password : string) => {
    return await signInWithEmailAndPassword(auth , email , password);
+}
+
+export const logoutUser = async() => {
+    await signOut(auth);
+    AsyncStorage.clear();
+    return
 }
